e3b9209d6dc7dfe3fec921c48bab97d658b24f51
muktar abdela <muktarhassen6@gmail.com>
2023-11-23 20:45:56 +0300

    last

--- client/src/Axios.js
+++ client/src/Axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-    baseURL: 'https://gabiskin.onrender.com',
+    baseURL: 'https://gabiskin.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
